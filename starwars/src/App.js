53ae641622da207fca6b1c19e1a4e166dc78bc21
Joseph Justin Opoku Mensah <josephjustin.om@gmail.com>
2024-07-24 18:37:11 +0000

    Completes MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import '../node_modules/bulma/css/bulma.css';
 import './App.css';
+import Character from './components/Character';
+import {useState, useEffect} from "react";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +12,23 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+      const fetchCharacters = async() => {
+          const response = await axios.get(`https://swapi.dev/api/people/`);
+          const data = await response.data.results;
+          setCharacters(data)
+      }
+      fetchCharacters()
+  }, [])
+
+  console.log(characters)
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
+    <div className="container is-fluid App">
+      <h1 className="Header is-size-3">Characters</h1>
+      <Character characters={characters} />
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,10 @@
-// Write your Character component here
+function Character({characters}) {
+    const renderedCharacters = characters.map(character => <div key={character.created} 
+            className="is-flex is-justify-content-space-between block is-size-4 has-text-black container is-max-desktop">
+            <span>{character.name}</span><span>{character.birth_year}</span>
+            </div>)
+    
+    return <div>{renderedCharacters}</div>
+}
+
+export default Character;
